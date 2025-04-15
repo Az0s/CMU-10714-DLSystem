ae9493d16e7db8887f08801bb0d4e94bc6d290dd
Az0s <b924258@outlook.com>
2025-04-15 22:25:41 +0800

    [hw2Q2] Implement LogSoftmax and LogSumExp operations with gradient calculations;

--- hw2/python/needle/ops/ops_logarithmic.py
+++ hw2/python/needle/ops/ops_logarithmic.py
@@ -10,13 +10,36 @@ import numpy as array_api
 class LogSoftmax(TensorOp):
     def compute(self, Z):
         ### BEGIN YOUR SOLUTION
-        raise NotImplementedError()
+        self.axes = (1,)
+        return Z - array_api.broadcast_to(
+            array_api.reshape(
+                logsumexp(Tensor(Z), self.axes).realize_cached_data(),
+                (*Z.shape[:-1], 1),
+            ),
+            Z.shape,
+        )
         ### END YOUR SOLUTION
 
     def gradient(self, out_grad, node):
-        ### BEGIN YOUR SOLUTION
-        raise NotImplementedError()
-        ### END YOUR SOLUTION
+        Z = node.inputs[0].realize_cached_data()
+        maxZ = array_api.max(Z, self.axes)
+        maxZmatShape = tuple(
+            [1 if d in self.axes else Z.shape[d] for d in range(len(Z.shape))]
+        )
+        stableExpZ = array_api.exp(
+            Z - array_api.broadcast_to(array_api.reshape(maxZ, maxZmatShape), Z.shape)
+        )
+        sumExp = array_api.sum(stableExpZ, self.axes)
+        s = stableExpZ / array_api.broadcast_to(
+            array_api.reshape(sumExp, maxZmatShape), Z.shape
+        )
+        # TODO why sum out_grad?
+        sum_out_grad = array_api.sum(out_grad.realize_cached_data(), self.axes)
+        sum_out_grad_broadcasted = array_api.broadcast_to(
+            array_api.reshape(sum_out_grad, maxZmatShape), Z.shape
+        )
+        grad = out_grad.realize_cached_data() - s * sum_out_grad_broadcasted
+        return Tensor(grad)
 
 
 def logsoftmax(a):
@@ -26,18 +49,44 @@ def logsoftmax(a):
 class LogSumExp(TensorOp):
     def __init__(self, axes: Optional[tuple] = None):
         self.axes = axes
+        self.stableExpZ = None
+        self.sumExp = None
+        self.maxZmatShape = None
 
     def compute(self, Z):
         ### BEGIN YOUR SOLUTION
-        raise NotImplementedError()
+        if not self.axes:
+            self.axes = tuple(range(len(Z.shape)))
+        if any(x < 0 for x in self.axes):
+            self.axes = tuple(x if x >= 0 else len(Z.shape) + x for x in self.axes)
+        maxZ = array_api.max(Z, self.axes)
+        self.maxZmatShape = tuple(
+            [1 if d in self.axes else Z.shape[d] for d in range(len(Z.shape))]
+        )
+        self.stableExpZ = array_api.exp(
+            Z
+            - array_api.broadcast_to(
+                array_api.reshape(maxZ, self.maxZmatShape), Z.shape
+            )
+        )
+        self.sumExp = array_api.sum(self.stableExpZ, self.axes)
+        return array_api.log(self.sumExp) + maxZ
         ### END YOUR SOLUTION
 
     def gradient(self, out_grad, node):
+        # NOTE the numerically stable log-sum-exp function formula shouldn't affect its derivative
         ### BEGIN YOUR SOLUTION
-        raise NotImplementedError()
+        return Tensor(
+            self.stableExpZ
+            * array_api.broadcast_to(
+                array_api.reshape(
+                    out_grad.realize_cached_data() / self.sumExp, self.maxZmatShape
+                ),
+                self.stableExpZ.shape,
+            )
+        )
         ### END YOUR SOLUTION
 
 
 def logsumexp(a, axes=None):
     return LogSumExp(axes=axes)(a)
-
